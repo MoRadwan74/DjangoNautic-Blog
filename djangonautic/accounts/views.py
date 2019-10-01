f6d62baf8a6594a966cfd0ce8109ed26fda51bc7
Radwan <mo.ahmed.radwan74@gmail.com>
2019-10-01 17:03:43 +0200

    logging users out

--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def signup_view(request):
@@ -29,3 +29,8 @@ def login_view(request):
         # Send login form and render it in the browser
         form = AuthenticationForm()
     return  render(request, 'accounts/login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('articles:list')
