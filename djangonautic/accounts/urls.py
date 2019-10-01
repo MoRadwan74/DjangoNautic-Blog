72a10ffbd38c5a02a7521c671b5e7c06928eb706
Radwan <mo.ahmed.radwan74@gmail.com>
2019-10-01 16:38:21 +0200

    Adding login from Login and Signup forms

--- djangonautic/accounts/urls.py
+++ djangonautic/accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.signup_view, name="signup")
+    path('signup/', views.signup_view, name="signup"),
+    path('login/', views.login_view, name="login"),
 ]
--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST) # Push the data to the form instance created with UserCreationForm.
         if form.is_valid():
-            form.save() # If the form is valid, save it to the database and create new user.
+            user = form.save() # If the form is valid, save it to the database and create new user.
             # Log the user in
+            login(request, user)
             return redirect('articles:list') # When logged redirect the user to articles list.
 
     else:   # If the request if 'GET'
         form = UserCreationForm() # Create form instance and send it to the render method below.
 
     return render(request, 'accounts/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # Log in the user
+            user = form.get_user()
+            login(request, user)
+            return redirect('articles:list')
+    else:
+        # Send login form and render it in the browser
+        form = AuthenticationForm()
+    return  render(request, 'accounts/login.html', {'form': form})
